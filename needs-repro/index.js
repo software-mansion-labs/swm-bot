bd6275b8144be32ab788073ca3bd9d9eef5a3dff
kacperkapusciak <kapusciak.k@gmail.com>
2021-12-23 10:38:22 +0100

    Remove comment when repro-provided

--- needs-repro/index.js
+++ needs-repro/index.js
@@ -26,6 +26,10 @@ async function run() {
 
     const reproValidator = new ReproValidator(body, user);
 
+    const comments = await octokit.rest.issues.listComments(issueData);
+
+    const comment = comments.data.find((comment) => comment.body === needsReproResponse);
+
     // Code adopted from https://github.com/react-navigation/react-navigation/blob/main/.github/workflows/check-repro.yml
     if (reproValidator.isReproValid()) {
       await octokit.rest.issues.addLabels({
@@ -40,6 +44,19 @@ async function run() {
         });
       } catch (error) {
         if (!/Label does not exist/.test(error.message)) {
+          throw error;
+        }
+      }
+
+      if (!comment) return;
+
+      try {
+        await octokit.rest.issues.deleteComment({
+          ...issueData,
+          comment_id: comment.id,
+        });
+      } catch (error) {
+        if (!/Comment does not exist/.test(error.message)) {
           throw error;
         }
       }
@@ -60,11 +77,7 @@ async function run() {
         }
       }
 
-      const comments = await octokit.rest.issues.listComments(issueData);
-
-      if (comments.data.some((comment) => comment.body === needsReproResponse)) {
-        return;
-      }
+      if (comment) return;
 
       await octokit.rest.issues.createComment({
         ...issueData,
