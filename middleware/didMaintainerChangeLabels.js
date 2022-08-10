b20b916680a206feb58eb19ea41ccc66bb7b432f
kacperkapusciak <kacper.kapusciak@swmansion.com>
2022-08-10 12:37:20 +0200

    Draft didMaintainerChangeLabels

--- middleware/didMaintainerChangeLabels.js
+++ middleware/didMaintainerChangeLabels.js
@@ -1,6 +1,47 @@
-async function didMaintainerChangeLabels(ctx, next) {
-  // noop
+import { graphql } from '@octokit/graphql';
+
+async function didMaintainerChangeLabels({ issueData }, next) {
+  const { owner, repo, issue_number: isseuNumber } = issueData;
+
+  const { data } = await graphql({
+    query: `query data($owner: String!, $repo: String!, $isseuNumber: Int!) {
+      repository(owner: $owner, name: $repo) {
+        issue(number: $isseuNumber) {
+          timelineItems(first: 250) {
+            edges {
+              node {
+                ... on LabeledEvent {
+                  actor {
+                    login
+                    ... on Bot {
+                      id
+                    }
+                  }
+                }
+                ... on UnlabeledEvent {
+                  actor {
+                    login
+                    ... on Bot {
+                      id
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }`,
+    owner,
+    repo,
+    isseuNumber,
+  });
+
+  const { timelineItems } = data.repository.issue;
+
+  console.log(timelineItems);
+
   next();
 }
 
-module.exports = didMaintainerChangeLabels;
+export default didMaintainerChangeLabels;
