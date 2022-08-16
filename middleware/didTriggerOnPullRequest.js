fde2895d3b9437384eee9161ded400d45e694dad
kacperkapusciak <kacper.kapusciak@swmansion.com>
2022-08-16 09:46:39 +0200

    Fix didTriggerOnPullRequest

--- middleware/didTriggerOnPullRequest.js
+++ middleware/didTriggerOnPullRequest.js
@@ -4,7 +4,7 @@ async function didTriggerOnPullRequest({ githubContext }, next) {
   const { payload } = githubContext;
 
   // Skip actions on pull requests
-  if (payload.issue.pull_request) {
+  if (payload?.issue?.pull_request) {
     core.notice('Action triggered by a comment added on a pull request. Skipping...');
     return;
   }
